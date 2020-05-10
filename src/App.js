61c4af0167b27c63dd1d88dda2eaae8a3efba864
Arman Ali <armanali12371@gmail.com>
2020-05-10 13:46:54 +0530

    covid 19 tracker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import Particles from 'react-particles-js';
+import { Cards, CountryPicker, Chart, Footer } from './components';
+import { fetchData } from './api/';
+import styles from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import covid19_logo from './images/covid19_logo.png';
+const particlesOptions = {
+      particles: {
+        number: {
+          value: 100,
+          density: {
+            enable: true,
+            value_area: 1000
+          }
+        }
+      }
+    }
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+      <Particles ClassName={styles.container} style={{position: 'fixed',zIndex:-1,top: 0, right: 0, bottom: 0, left: 0}}
+           params={particlesOptions}
+           />
+        <img className={styles.image} src={covid19_logo} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+        <Footer ClassName='footer' />
+      </div>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+const Footer = () =>{
+  return(
+    <div className={styles.footer}>
+    <p className={styles.footerName}>Design & Developed By Arman Ali</p>
+    </div>
+  )
+}
+export default Footer;
